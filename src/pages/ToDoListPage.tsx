550ab0a37397aea42b4eb76894a0fa79a6b97cc4
Alexander <soulfromsalem@gmail.com>
2026-03-26 04:08:25 +0600

    ver 1.3

--- src/pages/ToDoListPage.tsx
+++ src/pages/ToDoListPage.tsx
@@ -1,14 +1,42 @@
-import { Header } from "../components/Header/Header";
-import { Form } from "../components/Form/Form";
-import { ToDoList } from "../components/ToDoList/ToDoList";
-import { ToDoListItem } from "../components/ToDoList/ToDoListItem/ToDoListItem";
+import { Header } from "../components/Header/Header"
+import { Form } from "../components/Form/Form"
+import { ToDoList } from "../components/ToDoList/ToDoList"
+import { ToDoListItem } from "../components/ToDoList/ToDoListItem/ToDoListItem"
+import { ToDo } from "../models/todo-item"
+import { useState } from "react"
 
 export const ToDoListPage = () => {
+    const [todos, setTodos] = useState<ToDo[]>([])
+    
+    const createNewToDo = (text: string) => {
+        const newToDo: ToDo = {
+            id: todos.length,
+            text: text,
+            isDone: false
+        }
+        setTodos([...todos, newToDo])
+    }
+
+    const deleteToDo = (toDoItem: ToDo) => {
+        const newTodos = todos.filter((item) => item.id !== toDoItem.id)
+        setTodos(newTodos)
+    }
+
+    const updateToDo = (toDoItem: ToDo) => {
+        const newTodos = todos.map((item) => {
+            if (item.id === toDoItem.id) {
+                item.isDone = !item.isDone
+            }
+            return item
+        })
+        setTodos(newTodos)
+    }
+
     return (
         <>
             <Header />
-            <Form />
-            <ToDoList />
+            <Form createNewToDo={createNewToDo} />
+            <ToDoList todos={todos} updateToDo={updateToDo} deleteToDo={deleteToDo} />
         </>
     )
 }
